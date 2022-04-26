717f334a95c808c57364a9dddee3b74d1591b04c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-26 15:33:03 -0600

    Refactor test for notification

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   describe 'creation' do
     it 'can be created' do
-      notification = FactoryBot.create(:notification)
+      notification = FactoryBot.build_stubbed(:notification)
       expect(notification).to be_valid
     end
   end
