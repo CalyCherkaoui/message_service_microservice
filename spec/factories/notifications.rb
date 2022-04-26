1cb818690b0726d014e69894e35ef55fea26090c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-26 12:23:26 -0600

    Add factories

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :notification do
-    phone { "MyString" }
-    body { "MyText" }
-    source_app { "MyString" }
+    phone { "123-123-1234" }
+    body { "My Message" }
+    source_app { "some_app" }
+  end
+
+  factory :secobd_notification do
+    phone { "123-123-1234" }
+    body { "My Message notif 2" }
+    source_app { "some_app" }
   end
 end
