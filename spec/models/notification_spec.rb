047b85496074dcdc58ad270519e80dd326999266
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-26 15:35:32 -0600

    Test validations for notification creation

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Notification, type: :model do
       expect(notification).to be_valid
     end
   end
+
+  describe 'validations' do
+    it 'can be created if valid' do
+      notification = FactoryBot.build_stubbed(:notification)
+      notification.phone = nil
+      notification.body = nil
+      notification.source_app = nill
+      expect(notification).to_not be_valid
+    end
+  end
 end
