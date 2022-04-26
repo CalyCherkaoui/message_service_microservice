7ab62b9238aa57277bd9235f790c68e0dc513098
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-26 16:47:30 -0600

    Integration test for notification creation

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it "create a notification" do
+    headers = {
+      "ACCEPT" => "application/json"
+    }
+
+    post "/notifications",
+    {
+      notification: {
+        phone: "123-123-1234",
+        body: "Message",
+        source_app: "app_name"
+      }
+    },
+    headers
+    
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:created)
   end
 end
