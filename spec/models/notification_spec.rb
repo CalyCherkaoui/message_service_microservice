9e6b15261a5731ce13d4a3a27ff344204c51aff0
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-26 12:09:58 -0600

    Testing creation of Notification

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   describe 'creation' do
     it 'can be created' do
       notification = Notification.create(phone: "123-123-1234", body: "My message", source_app: "the_app")
