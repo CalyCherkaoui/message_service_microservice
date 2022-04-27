41cfd9d028ec5825af9bebd8c1fc7e9f1d13ff5a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-27 16:09:45 -0600

    Add notification view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,5 @@
 class NotificationsController < ApplicationController
   def create
-    Notification.create(params.permit(:phone, :body, :source_app))
+    @notification = Notification.create(params.permit(:phone, :body, :source_app))
   end
 end
