3c6f75efbf8ed2837a5f0ee65d4810c75352c834
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-27 16:12:00 -0600

    Implement json response to create action

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,17 @@
 class NotificationsController < ApplicationController
   def create
     @notification = Notification.create(params.permit(:phone, :body, :source_app))
+
+    respond_to do |format|
+      format.json{
+        render action: 'show', status: :created, location: @notification
+      }
+    end
+
   end
+
+  def show
+  end
+
 end
+
