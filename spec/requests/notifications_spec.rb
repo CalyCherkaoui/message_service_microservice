8838de297d89269048eeda23a2d72466a67fd9de
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-04-29 18:12:07 -0600

    Integrating error status code testing

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -19,4 +19,22 @@ RSpec.describe "Notifications", type: :request do
     # expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:created)
   end
+
+  it "renders an error status if the notification was not created" do
+    headers = {
+      "ACCEPT" => "application/json",
+      "Content-Type" => "application/json"
+    }
+
+    post "/notifications", 
+    :params => {
+      :notification => {
+        :phone => "123-123-1234",
+        :body => "Message",
+      }
+    }, :headers => headers
+    
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
 end
